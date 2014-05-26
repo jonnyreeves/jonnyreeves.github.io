8d1c9c62798d4709b699994fc1c2cba92417b26d
Jonny Reeves <git@jonnyreeves.co.uk>
2014-05-26 13:38:35 +0100

    Updated config

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,16 +16,24 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS = (
+	('Github', 'http://github.com/jonnyreeves'),
+	('Stack Overflow', 'http://stackoverflow.com/users/227349/jonnyreeves'),
+	('+JonnyReeves', 'https://plus.google.com/+JohnReeves')
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+
+# SVBHack Theme Settings
+THEME="../pelican-themes/pelican-svbhack"
+USER_LOGO_URL=SITEURL + '/images/avatar.jpg'
